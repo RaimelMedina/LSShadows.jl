a8eeaf176c9fd9f02c4b377aa01e36e29a8c9ea5
Documenter.jl <documenter@juliadocs.github.io>
2023-05-05 12:00:43 +0000

    build based on 644a7ef

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = LSShadows","category":"page"},{"location":"#LSShadows","page":"Home","title":"LSShadows","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for LSShadows.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [LSShadows]","category":"page"}]
+}
